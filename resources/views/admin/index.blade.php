fbd87d59e8fafe6cdea7776d58c634771633b858
DawidBarnas <dawidbarnas1998@gmail.com>
2022-05-10 13:07:18 +0200

    1.4 - Strona glowna oraz nazewnictwo

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
 
-SIEMANKO
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Panel administracyjny') }}</div>
+                <div class="card-body">
+                   
+                    
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
